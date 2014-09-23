9eb89f372cf3b6bc008e8a8ae9c441115f0a50f4
Kartik Kapila <kkapila@blackberry.com>
2014-09-23 15:39:58 -0400

    Commit 4

--- crawler.py
+++ crawler.py
@@ -4,7 +4,9 @@ import sys
 from urlparse import urljoin
 from bs4 import BeautifulSoup
 from sets import Set
+import time
 
+start_time = time.time()
 ###### INITIALIZATION ######
 frontier = []
 explored_set = []
@@ -27,7 +29,8 @@ def findAbsolutePath(relative_path) :
 	return urljoin(base_url,relative_path)
 
 def isKeyPhrasePresent(soup, absolute_path) :
-
+	if keyPhrase == "" :
+		return True
 	soup = BeautifulSoup(urllib2.urlopen(absolute_path).read())
 	text = soup.get_text()
 	if keyPhrase in text :
@@ -40,7 +43,7 @@ def findChildren(soup,depth) :
 		if isValidUrl(relative_path) :
 			absolute_path = findAbsolutePath(relative_path)
 			candidate = (absolute_path,depth)
-			if depth <= 2 and absolute_path not in common_set and isKeyPhrasePresent(soup, absolute_path) :
+			if depth <= 2 and absolute_path not in common_set and  isKeyPhrasePresent(soup, absolute_path) :
 				frontier.append(candidate)
 				common_set.add(absolute_path)
 
@@ -50,15 +53,20 @@ while len(frontier) != 0 :
 	leaf_node = frontier.pop(0)	
 	print len(frontier)
 	if leaf_node[1] + 1 > 2:
+		explored_set.append(leaf_node[0])
 		break
 	explored_set.append(leaf_node[0])
 	common_set.add(leaf_node[0])
 	soup = BeautifulSoup(urllib2.urlopen(leaf_node[0]).read())
 	findChildren(soup,leaf_node[1] + 1)
 
-count = 0
+for f in frontier :
+	explored_set.append(f[0])
+
+count = 1
 print len(frontier)
 for f in explored_set: 
 	print count
 	print f
 	count += 1
+print(time.time() - start_time)
