b81afd8ff60d0dfc70edc7918de4aafaf7c25e5a
kartikkapila <kartikkapila@kartiks-mbp-2.cable.rcn.com>
2014-09-23 22:20:44 -0400

    Commit 4

--- crawler.py
+++ crawler.py
@@ -4,7 +4,10 @@ import sys
 from urlparse import urljoin
 from bs4 import BeautifulSoup
 from sets import Set
+import time
+print "Starting" + "Program"
 
+start_time = time.time()
 ###### INITIALIZATION ######
 frontier = []
 explored_set = []
@@ -27,17 +30,27 @@ def findAbsolutePath(relative_path) :
 	return urljoin(base_url,relative_path)
 
 def isKeyPhrasePresent(soup, absolute_path) :
-
+	if keyPhrase == "" :
+		return True
 	soup = BeautifulSoup(urllib2.urlopen(absolute_path).read())
 	text = soup.get_text()
 	if keyPhrase in text :
 		return True
 	return False
 
+def removeHash(relative_path) :
+	if relative_path.find('#') != -1 :
+		index = relative_path.index('#')
+		return relative_path[0 : index]
+	return relative_path
+
 def findChildren(soup,depth) :
 	for anchor in soup.find_all('a') :
 		relative_path = anchor.get('href')	
 		if isValidUrl(relative_path) :
+			print "relative path:" + str(relative_path)
+			relative_path = removeHash(str(relative_path))
+			print "relative path:" + str(relative_path)
 			absolute_path = findAbsolutePath(relative_path)
 			candidate = (absolute_path,depth)
 			if depth <= 2 and absolute_path not in common_set and isKeyPhrasePresent(soup, absolute_path) :
@@ -48,17 +61,25 @@ def findChildren(soup,depth) :
 
 while len(frontier) != 0 :
 	leaf_node = frontier.pop(0)	
-	print len(frontier)
 	if leaf_node[1] + 1 > 2:
+		explored_set.append(leaf_node[0])
 		break
 	explored_set.append(leaf_node[0])
 	common_set.add(leaf_node[0])
 	soup = BeautifulSoup(urllib2.urlopen(leaf_node[0]).read())
 	findChildren(soup,leaf_node[1] + 1)
 
+for f in frontier :
+	explored_set.append(f[0])
+
+output = open('output.txt','w')
 count = 0
-print len(frontier)
-for f in explored_set: 
-	print count
-	print f
+for item in explored_set: 
 	count += 1
+	output.write(item)
+	output.write('\n')
+
+output.write('Some Additional Information \n')
+output.write("Time Taken:" + str(time.time() - start_time) + "\n")
+output.write("No of Links:" + str(count))
+output.close()
