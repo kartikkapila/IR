4213f9e0cd19a98ebceaa2e489b4d15ec2a9ffe9
Kartik Kapila <kkapila@blackberry.com>
2014-11-05 17:17:54 -0500

    Updates

--- Zipf/heap.py
+++ Zipf/heap.py
@@ -1,2 +1,19 @@
+from sets import Set
 input_file = open("output.txt","r")
+input_list = []
+for word in input_file :
+        input_list.append(word.split("\n").pop(0))
+output_file = open("total_words_vs_unique_words.txt","w")
+unique_words = Set()
+unique_words_count = 0
+words_processed = 0
+for word in input_list :
+	words_processed += 1
+	if word not in unique_words :
+		unique_words.add(word)
+		unique_words_count += 1
+		output_file.write(str(words_processed) + " " + str(unique_words_count) + "\n")
+	else :
+		output_file.write(str(words_processed) + " " + str(unique_words_count) + "\n")
 
+	
